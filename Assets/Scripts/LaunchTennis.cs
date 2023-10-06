fd021a2d48b135a46210ec3c9b704ce7bcb88238
05eze <esintim2022@elam.co.uk>
2023-10-06 12:45:58 +0100

    Launching Tennis ball

---
+++ Assets/Scripts/LaunchTennis.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchTennis : MonoBehaviour
+{
+    public Transform launchPoint;
+    //public 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
